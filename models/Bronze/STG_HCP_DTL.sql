e427b56ae27bccb08a39b15a70e32aeadc59400c
Nikita Bankar <nsbankar501@gmail.com>
2026-05-20 07:09:36 +0000

    First data model demo project

---
+++ models/Bronze/STG_HCP_DTL.sql
@@ -0,0 +1,13 @@
+/* creating staging for HCPs */
+
+{{ config(materialized='table') }}
+
+with source_data as (
+
+    select hcp_npi,hcp_first_name||' '||hcp_last_name as hcp_name,hcp_zip,hcp_team,hcp_territory
+    from RAW.DRUG_SALES.LDG_HCP_DETAIL
+
+)
+
+select *
+from source_data
