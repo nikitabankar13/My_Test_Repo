9d97c470c4c799999c45e0c81250c0390f2a92a2
Nikita Bankar <nsbankar501@gmail.com>
2026-05-27 11:11:25 +0000

    added tests and identifiers

--- models/Bronze/STG_HCP_DTL.sql
+++ models/Bronze/STG_HCP_DTL.sql
@@ -5,8 +5,7 @@
 with source_data as (
 
     select hcp_npi,hcp_first_name||' '||hcp_last_name as hcp_name,hcp_zip,hcp_team,hcp_territory
-    from {{ source('raw_data_tables', 'LDG_HCP_DETAIL') }}
-
+    from {{ source('raw_data_tables', 'raw_hcp') }}
 )
 
 select *
